445cf3dac3c9345edbf5047e9f03e2c2d4a273dd
guyaston <guy.aston@themarker.com>
2018-04-09 18:32:42 +0300

    setup passport

--- index.js
+++ index.js
@@ -2,22 +2,38 @@
 
 // * Imports _______________________________________________________________ //
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys");
 
 // * Configurations ________________________________________________________ //
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
 /* =============================== ROUTING & MIDDLEWARE ================================ */
 
 // * Routing _______________________________________________________________ //
-app.get("/", (req, res) => {
-  sendResponse(res);
-});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 // * Middleware ____________________________________________________________ //
-function sendResponse(resObject) {
-  resObject.send({ hi: "there" });
-}
 
 /* ======================================= MAIN ======================================== */
 
