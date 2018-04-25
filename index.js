bdf02c043fd20d688cb41bdb066a1ad276c652fa
guyaston <guy.aston@themarker.com>
2018-04-25 19:03:41 +0300

    refactor server code

--- index.js
+++ index.js
@@ -2,30 +2,16 @@
 
 // * Imports * //
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20");
-const keys = require("./config/keys");
+require('./services/passport');
 
 // * Configurations * //
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, accessToken => {
-  console.log(accessToken);
-}));
-
 /* =============================== ROUTING & MIDDLEWARE ================================ */
 
 // * Routing * //
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 // * Middleware * //
 
