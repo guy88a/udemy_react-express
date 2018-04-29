d9ec55ca0c408cfd22f5032b1d8d8164d251c5bb
guyaston <guy.aston@themarker.com>
2018-04-29 19:14:36 +0300

    connect to mongo DB

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 
 // * Imports * //
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require('./services/passport');
 
 // * Configurations * //
 const app = express();
+mongoose.connect(keys.mongoURI);
 const PORT = process.env.PORT || 5000;
 
 /* =============================== ROUTING & MIDDLEWARE ================================ */
