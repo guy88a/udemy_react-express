36d8e3d3c79770aef03ed32286895610fb265351
guyaston <guy.aston@themarker.com>
2018-05-02 13:10:27 +0300

    authenticate and set cookie

--- services/passport.js
+++ services/passport.js
@@ -10,18 +10,63 @@
 
 const passport = require('passport');
 const GoogleStrategy = require("passport-google-oauth20");
-const keys = require("../config/keys");
 const mongoose = require('mongoose');
+const keys = require("../config/keys");
 
 /* ================================== CONFIGURATIONS =================================== */
 
 const User = mongoose.model('users');
 
 // * Configurations * //
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User
+        .findById(id)
+        .then(user => {
+            done(null, user);
+        });
+})
+
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    new User({googleId: profile.id}).save();
+    User
+        .findOne({googleId: profile.id})
+        .then(existingUser => {
+            if (existingUser) {
+                done(null, existingUser);
+                // we already have a record with the given profileID
+            } else {
+                // we don´t have a user record with this ID, make a new record
+                new User({googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }));
+
+/* ==================================== FUNCTIONS ====================================== */
+
+function handleSignin(user, done) {
+    if (user) {
+        //user already exists:
+        done(null, existingUser);
+        console.log("user already exists");
+    } else {
+        //create a new user:
+        new User({googleId: profile.id})
+            .save()
+            .then((user) => {
+                done(null, user);
+                console.log("new user created");
+            });
+    }
+}
