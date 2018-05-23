a279aa1a6e22454cf12132bcd5fb3ca9e63f1cb9
guyaston <guy.aston@themarker.com>
2018-05-23 16:38:29 +0300

    setup reducers poiplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-    <App/>, document.getElementById("root"));
+    <Provider store={store}><App/></Provider>, document.getElementById("root"));
