b33f170d6ef3893d227c9aaa0ccc2029bdf50e38
guyaston <guy.aston@themarker.com>
2018-05-30 17:35:32 +0300

    get logged status for header + callback redirect on login S07L83

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {FETCH_USER} from './types';
 
-const fetchUser = () => {
+export const fetchUser = () => {
     return async function (dispatch) {
         const user = await axios.get('/api/current_user');
         dispatch({type: FETCH_USER, payload: user});
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login with Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+        }
+    }
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a href="/" className="left brand-logo">Emaily</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li>
-                            <a href="/surveys">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="/surveys/new">New Survey</a>
-                        </li>
-                        <li>
-                            <a href="/google/auth">Login</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -23,4 +35,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+    return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
